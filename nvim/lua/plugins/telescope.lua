da84d584c98e24541a329cc1c0ded7b79acb237e
jacobm-gavin <116292552+jacobm-gavin@users.noreply.github.com>
2025-08-18 21:02:38 -0400

    add ui rice

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,9 +15,10 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>p", builtin.find_files, {})
       vim.keymap.set("n", "<leader>rg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+      vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
       require("telescope").load_extension("ui-select")
