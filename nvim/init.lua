69c2f259ecd31bd10ab653059782fa3f4a605339
jacobm-gavin <116292552+jacobm-gavin@users.noreply.github.com>
2025-10-07 10:25:49 -0400

    (NVIM) Add keymap for yanking to system clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,3 +16,4 @@ require("lazy").setup("plugins")
 
 vim.cmd("colorscheme rose-pine")
 vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChatToggle<cr>", { desc = "Toggle Copilot Chat" })
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "yank to system clipboard" })
